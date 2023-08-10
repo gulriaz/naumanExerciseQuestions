05c4ce6343571d25ac56e498f54618cac0716752
gulraiz <ingrgulraiz@gmail.com>
2023-08-10 14:08:33 +0500

    question 20, A3 switches all done.

--- src/AssignmentThreeSwitch/MainSwitch.java
+++ src/AssignmentThreeSwitch/MainSwitch.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class MainSwitch {
     public static void main(String[] args) {
-
+//
         //question20
         /*SwitchAssignment s = new SwitchAssignment();
         Scanner sc = new Scanner(System.in);
